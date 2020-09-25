9b9b0f310e1b4025324b364752e87b817f08915c
a9289 <67312684+a9289@users.noreply.github.com>
2020-09-25 22:01:46 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,32 +1,7 @@
 #!/bin/sh
 # ruby2 new configuration
 base64 > /etc/ruby2/config << EOF
-{
-  "inbounds": [
-  {
-    "port": $PORT,
-    "listen":"127.0.0.1",
-    "protocol": "vmess",
-    "settings": {
-      "clients": [
-        {
-          "id": "ad806487-2d26-4636-98b6-ab85cc8521f7",
-          "alterId": 64       
-        }
-      ]
-    },
-    "streamSettings": {
-      "network": "ws"
-    }
-  }
-  ],
-  "outbounds": [
-  {
-    "protocol": "freedom",
-    "settings": {}
-  }
-  ]
-}
+$CONFIG_JSON
 EOF
 # Run ruby2
 cat /etc/ruby2/config | base64 -d > /etc/ruby2/config.json
